b6dedf5d4fa974bd8612fa74d2706dcc52b68d8b
kunilovsky <kunilovsky@gmail.com>
2023-04-18 12:07:50 +0500

    Invest page - Investment

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,16 +19,36 @@ for (let i = 0; i < links.length; i++) {
 
 const openNavMobile = document.getElementById('open-nav');
 const iconOpenNav = document.getElementById('icon-open-nav');
-openNavMobile.addEventListener('click', ()=> {
-  for(let value of links) value.classList.toggle('nav-mobile-open');
+openNavMobile.addEventListener('click', () => {
+  for (let value of links) value.classList.toggle('nav-mobile-open');
   iconOpenNav.classList.toggle('close-nav');
-})
+});
 
 //Delete to Laravel
 let urlDash = window.location.href;
 let dashStart = document.getElementById('dash-start');
-document.addEventListener("DOMContentLoaded", ()=> {
-    if(url.indexOf('http://127.0.0.1:5500/dashboard.html') != -1) {
-        dashStart.classList.add('nav-active');
-    }
-})
+document.addEventListener('DOMContentLoaded', () => {
+  if (url.indexOf('http://127.0.0.1:5500/dashboard.html') != -1) {
+    dashStart.classList.add('nav-active');
+  }
+});
+
+//Invested page show/hide
+const btnPageInvestment = document.getElementById('investments');
+const btnPageInvested = document.getElementById('invested');
+const pageInvestment = document.getElementById('page-investment');
+const pageInvested = document.getElementById('page-invested');
+
+btnPageInvestment.addEventListener('click', () => {
+  btnPageInvestment.classList.add('invest-page-active');
+  btnPageInvested.classList.remove('invest-page-active');
+  pageInvestment.classList.add('invest-active');
+  pageInvested.classList.remove('invest-active');
+});
+
+btnPageInvested.addEventListener('click', () => {
+  btnPageInvestment.classList.remove('invest-page-active');
+  btnPageInvested.classList.add('invest-page-active');
+  pageInvestment.classList.remove('invest-active');
+  pageInvested.classList.add('invest-active');
+});
